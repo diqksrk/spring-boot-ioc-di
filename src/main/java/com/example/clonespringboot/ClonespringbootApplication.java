044de3098b88f61e0ac819da30371b8eb798d73e
diqksrk <diqksrk123@naver.com>
2022-07-10 21:11:01 +0900

    git init

---
+++ src/main/java/com/example/clonespringboot/ClonespringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.clonespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonespringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClonespringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clonespringboot/ClonespringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clonespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClonespringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
